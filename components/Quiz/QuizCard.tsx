df414baf1754cc763d48b5961eb1100ff7a6c9e2
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-26 03:42:20 +0500

    removed useless code and added the index context to keep on changing questions

--- components/Quiz/QuizCard.tsx
+++ components/Quiz/QuizCard.tsx
@@ -1,47 +1,32 @@
 import { Quiz } from '@/utils/BlogInterface'
-import React from 'react'
-
+import { UserContext } from '@/utils/Context'
+import React, { useContext } from 'react'
 const Questions = ({ quiz }: { quiz: Quiz }) => {
-  // const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+  const { currentQuestionIndex } = useContext(UserContext)
   // const [selectedOption, setSelectedOption] = useState('')
-
-  // useEffect(() => {
-  //   // Reset the selected option when the question changes
-  //   setSelectedOption('')
-  // }, [currentQuestionIndex])
-  // const handleAnswerSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //     setSelectedOption(e.target.value)
-  //   }
-  //   const handleNextQuestion = () => {
-  //     if (currentQuestionIndex < QuizData.length - 1) {
-  //       setCurrentQuestionIndex(currentQuestionIndex + 1)
-  //     } else {
-  //       alert('You have completed the quiz!')
-  //     }
-  //   }
-  //   const currentQuestion = QuizData[currentQuestionIndex]
-
   return (
     <>
-      {/* Ensure Questions is defined before mapping */}
       {quiz.Questions &&
-        quiz.Questions.map((question, index) => (
-          <div key={index} className="mb-6 text-black">
-            <h3 className="font-semibold mb-2">
-              Q{index + 1}: {question.Question}
-            </h3>
-            <ul className="list-disc pl-5">
-              {question.Options.map((option: any, idx: any) => (
-                <li key={idx} className="mb-1">
-                  {option}
-                </li>
-              ))}
-            </ul>
-            <p className="mt-2 text-sm text-gray-500">
-              Explanation: {question.Explanation}
-            </p>
-          </div>
-        ))}
+        quiz.Questions.map(
+          (question, index) =>
+            currentQuestionIndex == index && (
+              <div key={index} className="mb-6 text-black">
+                <h3 className="font-semibold mb-2">
+                  Q{index + 1}: {question.Question}
+                </h3>
+                <ul className="list-disc pl-5">
+                  {question.Options.map((option: any, idx: any) => (
+                    <li key={idx} className="mb-1">
+                      {option}
+                    </li>
+                  ))}
+                </ul>
+                <p className="mt-2 text-sm text-gray-500">
+                  Explanation: {question.Explanation}
+                </p>
+              </div>
+            )
+        )}
     </>
   )
 }
