0cc295d1ca9dc61a261792125c0b27d1349e2fbf
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-26 03:27:10 +0500

    Create page.tsx

---
+++ app/Quiz/[ID]/page.tsx
@@ -0,0 +1,52 @@
+'use client'
+import Loader from '@/components/Loader'
+import Questions from '@/components/Quiz/QuizCard'
+import { fetchDocumentByID } from '@/functions/Quiz/GettingAsingleDoc'
+import { Quiz } from '@/utils/BlogInterface'
+import { UserContext } from '@/utils/Context'
+import React, { useContext, useEffect, useState } from 'react'
+const QuizCard = ({ params }: { params: any }) => {
+  const [quiz, setQuizData] = useState<Quiz | null>(null)
+  const { loading, setLoading } = useContext(UserContext)
+  useEffect(() => {
+    const GetData = async () => {
+      setLoading(true)
+      try {
+        const data = await fetchDocumentByID(params.ID)
+        if (data) {
+          setQuizData(data)
+          //   //   console.log('DATA HAS BEEN FETCHED : ', data)
+          //   console.log(quiz?.Questions)
+        }
+      } catch (error) {
+        console.log('ERROR IN FUNCTION :', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+    GetData()
+  }, [params.ID, setLoading])
+
+  if (loading) {
+    return <Loader />
+  }
+  if (!quiz) {
+    return <div>No quiz found.</div>
+  }
+  return (
+    <div className="bg-white shadow-lg rounded-lg mx-auto my-5 shadow-blue-700 w-full md:w-2/3 lg:w-1/2 p-6">
+      <h2 className="text-2xl font-bold text-center mb-4">{quiz.Topic}</h2>
+      <p className="text-center text-gray-600 mb-6 flex flex-col">
+        <span>Number Of Questions : {quiz.NumberOfQuestions} </span>
+        <span>Difficulty: {quiz.Difficulty}</span>
+      </p>
+      <Questions quiz={quiz} />
+      <div className=" flex justify-end">
+        <button className=" bg-blue-400 px-4 text-white border-gray-100 rounded-lg">
+          Next
+        </button>
+      </div>
+    </div>
+  )
+}
+export default QuizCard
