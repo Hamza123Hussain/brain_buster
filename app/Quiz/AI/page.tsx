763385cd0593223c09b8d40d121e0b56d1358ee0
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-25 21:27:10 +0500

    removing unused code and making sure that this component according to the QUIZ APP

--- app/Quiz/AI/page.tsx
+++ app/Quiz/AI/page.tsx
@@ -3,25 +3,39 @@ import { UserContext } from '@/utils/Context'
 import React, { useContext, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import toast from 'react-hot-toast'
-import Loader from '@/components/Loader'
 import CreateBlogFields from '@/components/Blog/CreateBlogFields'
 import { QuizCreate } from '@/utils/QuizCreate'
+import { fetchAiQuestions } from '@/functions/Quiz/CreateAQuiz'
 const WriteWithAI = () => {
-  const { userData, loading, setLoading } = useContext(UserContext)
+  const Router = useRouter()
+  const { userData, setLoading, SetQuizData } = useContext(UserContext)
   const [CreateValue, SetValue] = useState<QuizCreate>({
     Topic: '',
     Image: null,
     DifficultyLevel: '',
     NumberOfQuestion: 3,
   })
-  const Router = useRouter()
-  const PlaceHolder = {
-    Topic: 'Enter A Reasonable Prompt to Generate A Blog Topic',
-    Text: 'Enter A Reasonable Prompt to Generate A Description For Your Blog',
-  }
-
-  if (loading) {
-    return <Loader />
+  const handleFetchQuestions = async () => {
+    setLoading(true)
+    try {
+      const result = await fetchAiQuestions(
+        CreateValue.Topic,
+        CreateValue.NumberOfQuestion,
+        userData.email,
+        CreateValue.DifficultyLevel,
+        userData.Name
+      )
+      if (result) {
+        console.log(result)
+        SetQuizData(result)
+        setLoading(false)
+        toast.success('QUIZ HAS BEEN MADE')
+        Router.push('/')
+      }
+    } catch (error) {
+      console.log('ERROR IN FUNCTION', error)
+      setLoading(false)
+    }
   }
   return (
     <div className="flex flex-col items-center bg-gray-100 p-8 rounded-lg shadow-lg max-w-3xl mx-auto my-10">
@@ -29,13 +43,12 @@ const WriteWithAI = () => {
         Create a New Blog With AI
       </h1>
       <div className="w-full space-y-6">
-        <CreateBlogFields
-          PlaceHolder={PlaceHolder}
-          CreateValue={CreateValue}
-          SetValue={SetValue}
-        />
+        <CreateBlogFields CreateValue={CreateValue} SetValue={SetValue} />
         <div className=" flex gap-2">
-          <button className="w-full bg-customBg text-white py-3 rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-dusty-rose">
+          <button
+            onClick={handleFetchQuestions}
+            className="w-full bg-customBg text-white py-3 rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-dusty-rose"
+          >
             Submit
           </button>
         </div>
