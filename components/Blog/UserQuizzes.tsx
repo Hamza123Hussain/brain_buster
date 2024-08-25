6c392077d90fa7600f08fe8b74f2f71559c3f915
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-25 21:41:05 +0500

    showing the major details of the quiz

--- components/Blog/UserQuizzes.tsx
+++ components/Blog/UserQuizzes.tsx
@@ -35,6 +35,25 @@ const UserQuizzes = () => {
           Make A Quiz With AI
         </button>
       </div>
+      <div className=" grid grid-cols-1 sm:grid-cols-2  gap-2">
+        {UserQUIZ.map((element) => (
+          <div
+            onClick={() => Router.push(`/Quiz/${element.ID}`)}
+            className="bg-white shadow-md rounded-lg p-4 w-full sm:w-[45vw]  cursor-pointer hover:bg-[#c2f1f1] transition-transform transform hover:scale-90"
+            key={element.ID}
+          >
+            <h3 className="text-xl font-semibold text-cyan-700 mb-2">
+              {element.Topic}
+            </h3>
+            <p className="text-gray-600 mb-1">
+              <strong>Difficulty:</strong> {element.Difficulty}
+            </p>
+            <p className="text-gray-600">
+              <strong>Questions:</strong> {element.NumberOfQuestions}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
