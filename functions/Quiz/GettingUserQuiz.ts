b7456c0604a96347693d15400db69e08bcb29912
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-25 21:28:09 +0500

    getting the quiz according to the user

---
+++ functions/Quiz/GettingUserQuiz.ts
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+export const fetchAiQuestions = async (userEmail: string) => {
+  try {
+    const response = await axios.get('http://localhost:8000/api/User/GetQuiz', {
+      params: { UserEmail: userEmail },
+    })
+
+    if (response.status === 200) {
+      // Return the AI questions data
+      return response.data
+    } else if (response.status === 404) {
+      // Handle case when no AI questions are found
+      throw new Error('No AI questions found for the given UserEmail')
+    }
+  } catch (error) {
+    // Handle errors during the fetch operation
+    console.error('Error fetching AI questions:', error)
+    throw new Error('Failed to fetch AI questions')
+  }
+}
