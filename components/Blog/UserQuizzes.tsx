03b462b4387f563947f3634b216bc42be4ddbbb6
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-26 04:25:07 +0500

    work has to be done here

--- components/Blog/UserQuizzes.tsx
+++ components/Blog/UserQuizzes.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/navigation'
 import { fetchAiQuestions } from '@/functions/Quiz/GettingUserQuiz'
 import { Quiz } from '@/utils/BlogInterface'
 import DisplayCard from '../Quiz/DisplayCard'
+import { FaRegFrown } from 'react-icons/fa'
 const UserQuizzes = () => {
   const { userData, setLoading, loading } = useContext(UserContext)
   const [UserQUIZ, setQUIZ] = useState<Quiz[]>([])
@@ -26,6 +27,7 @@ const UserQuizzes = () => {
   if (loading) {
     return <Loader />
   }
+
   return (
     <div className="flex flex-col p-2 px-4 justify-center items-center ">
       <div className="flex flex-col sm:flex-row gap-2 my-5">
@@ -36,11 +38,20 @@ const UserQuizzes = () => {
           Make A Quiz With AI
         </button>
       </div>
-      <div className=" grid grid-cols-1 sm:grid-cols-2  gap-2">
-        {UserQUIZ.map((element) => (
-          <DisplayCard key={element.ID} element={element} />
-        ))}
-      </div>
+      {UserQUIZ.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+          {UserQUIZ.map((element) => (
+            <DisplayCard key={element.ID} element={element} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-col justify-center mx-auto min-h-screen items-center mt-20">
+          <FaRegFrown size={100} className="text-4xl text-gray-500 mb-4" />
+          <p className="text-center text-lg md:text-4xl text-gray-500">
+            No Quizzes created yet.
+          </p>
+        </div>
+      )}
     </div>
   )
 }
