77f39b5927fe4e18885a86e6c204c62e13e727fe
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-25 22:03:46 +0500

    Getting All of the Quizzes Created

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,30 @@
 'use client'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Loader from '@/components/Loader'
 import { Quiz } from '@/utils/BlogInterface'
 import { UserContext } from '@/utils/Context'
 import { FaRegSadTear } from 'react-icons/fa'
-
+import { fetchAllAiQuiz } from '@/functions/Quiz/GettingALLQuiz'
 export default function Home() {
-  const { SetQuizData, QuizData } = useContext(UserContext)
-
+  const { userData, loading, setLoading } = useContext(UserContext)
+  const [QuizData, SetQuizData] = useState<Quiz[]>([])
+  useEffect(() => {
+    const GetQuizzes = async () => {
+      setLoading(true)
+      try {
+        const Data = await fetchAllAiQuiz(userData.UserID)
+        if (Data) {
+          console.log('API RESPONDED : ', Data)
+          SetQuizData(Data)
+          setLoading(false)
+        }
+      } catch (error) {
+        console.log('FUNCTION ERROR : ', error)
+        setLoading(false)
+      }
+    }
+    GetQuizzes()
+  }, [])
   if (QuizData.length === 0) {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen">
@@ -16,8 +33,13 @@ export default function Home() {
       </div>
     )
   }
-
+  if (loading) return <Loader />
+  //add title and icon here
   return (
-    <div className="flex flex-col items-center mx-auto my-5 justify-center min-h-screen p-2  rounded-xl"></div>
+    <div className="flex flex-col items-center mx-auto my-5 justify-center min-h-screen p-2  rounded-xl">
+      {QuizData.map((Quiz) => (
+        <div key={Quiz.ID}>{Quiz.Topic}</div>
+      ))}
+    </div>
   )
 }
