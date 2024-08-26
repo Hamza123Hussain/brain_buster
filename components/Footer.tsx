ca8567aa3ea3ea4996f2c4ae2fb832140d910f06
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-26 20:20:26 +0500

    added disclaimer in footer and made it smaller

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,20 +3,24 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-50 text-gray-500 py-1 border-t border-black  shadow-md px-2">
-      <div className="text-center flex  flex-col sm:flex-row  justify-between items-center ">
-        {' '}
-        <div className="flex  items-center">
+    <footer className="bg-gray-50 text-gray-500  border-t border-slate-200 shadow-md px-4">
+      <div className="text-center flex flex-col sm:flex-row justify-between  items-center">
+        {/* Logo and Title */}
+        <div className="flex items-center sm:mb-0">
           <Image
-            src={'/Logo.png'}
+            src="/Logo.png"
             alt="Logo"
-            width={50}
+            width={30}
             height={40}
-            className=" rounded-full"
+            className="rounded-full"
           />
-          <h4 className="text-xl font-bold mb-2">Brain Buster</h4>
+          <h4 className="text-xs font-bold ml-2">Brain Buster</h4>
         </div>
-        <p className=" text-sm sm:text-lg">
+        <p className="text-xs ">
+          Disclaimer: The AI-generated content might not be 100% accurate.
+        </p>
+        {/* Footer Text */}
+        <p className="text-xs">
           &copy; {new Date().getFullYear()} Brain Buster. All rights reserved.
         </p>
       </div>
