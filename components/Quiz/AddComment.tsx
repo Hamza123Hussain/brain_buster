1017ddfd6be2d7b903ef3d045615569919423cdd
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-27 15:45:10 +0500

    setting a loading state when adding a new comment

--- components/Quiz/AddComment.tsx
+++ components/Quiz/AddComment.tsx
@@ -2,14 +2,14 @@ import { NewComment } from '@/functions/Quiz/CreatingAComment'
 import { UserContext } from '@/utils/Context'
 import { CommentData, Quiz } from '@/utils/Quiz'
 import React, { useContext, useState } from 'react'
-
 const AddComment = () => {
-  const { QUIZDATA, setData, userData } = useContext(UserContext)
+  const { QUIZDATA, setData, userData, setLoading } = useContext(UserContext)
   const [newComment, setNewComment] = useState('')
 
   const handleAddComment = async (e: React.FormEvent) => {
     e.preventDefault()
     if (newComment.trim() && QUIZDATA) {
+      setLoading(true)
       try {
         const Data: CommentData = await NewComment(
           newComment,
@@ -18,19 +18,18 @@ const AddComment = () => {
           userData.imageUrl,
           userData.Name
         )
-
         setData((prev: Quiz) => ({
           ...prev,
           comments: [...(prev.comments || []), Data], // Initialize comments if undefined
         }))
-
+        setLoading(false)
         setNewComment('') // Clear the comment input field
       } catch (error) {
         console.error('Error adding comment:', error)
+        setLoading(false)
       }
     }
   }
-
   return (
     <form onSubmit={handleAddComment} className="flex gap-2 items-center mb-4">
       <input
@@ -49,5 +48,4 @@ const AddComment = () => {
     </form>
   )
 }
-
 export default AddComment
