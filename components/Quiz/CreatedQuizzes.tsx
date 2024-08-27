19224e733755f7be814e1ee17452009a7f61a933
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-28 03:54:18 +0500

    cleaning the componenet

--- components/Quiz/CreatedQuizzes.tsx
+++ components/Quiz/CreatedQuizzes.tsx
@@ -3,8 +3,9 @@ import { UserContext } from '@/utils/Context'
 import { Quiz } from '@/utils/Quiz'
 import React, { useContext, useEffect, useState } from 'react'
 import Loader from '../Loader'
-import { FaRegFrown } from 'react-icons/fa'
 import DisplayCard from './DisplayCard'
+import Empty from '../Empty'
+import { FaRegFrown } from 'react-icons/fa'
 const CreatedQuizzes = () => {
   const { userData, setLoading, loading } = useContext(UserContext)
   const [UserQUIZ, setQUIZ] = useState<Quiz[]>([])
@@ -16,9 +17,15 @@ const CreatedQuizzes = () => {
       }
       setLoading(true)
       try {
-        const Data = await fetchAiQuestions(userData.email)
-        if (Data) {
-          setQUIZ(Data)
+        const data = await fetchAiQuestions(userData.email)
+        if (data) {
+          // Sort the data by CreatedAt in descending order
+          const sortedData = data.sort((a: any, b: any) => {
+            const dateA = new Date(a.CreatedAt)
+            const dateB = new Date(b.CreatedAt)
+            return dateB.getTime() - dateA.getTime()
+          })
+          setQUIZ(sortedData)
         } else {
           console.warn('No quizzes found for the user')
         }
@@ -43,12 +50,12 @@ const CreatedQuizzes = () => {
           ))}
         </div>
       ) : (
-        <div className="flex flex-col justify-center mx-auto items-center mt-20">
-          <FaRegFrown size={100} className="text-4xl text-gray-500 mb-4" />
-          <p className="text-center text-lg md:text-4xl text-gray-500">
-            No Quizzes created yet.
-          </p>
-        </div>
+        <Empty
+          text="No Quizzes created yet."
+          icon={
+            <FaRegFrown size={100} className="text-4xl text-gray-500 mb-4" />
+          }
+        />
       )}
     </>
   )
