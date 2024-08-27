e1cedcdc8ce0d2aa5df6b6095f0a879c0a14f1be
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-27 15:43:54 +0500

    loader specifically for comments

---
+++ functions/Quiz/commentLoader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CommentLoader = () => {
+  return (
+    <div className=" flex  justify-center items-center mx-auto my-auto ">
+      <div className="loader"></div>
+    </div>
+  )
+}
+
+export default CommentLoader
