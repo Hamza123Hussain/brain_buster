b2ed8adff266ddb7fc38149d069b3f58d2dfef48
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-27 15:44:48 +0500

    only the user who posted the comment can delete it

--- components/Quiz/CommentCard.tsx
+++ components/Quiz/CommentCard.tsx
@@ -1,28 +1,27 @@
-import { UserContext } from '@/utils/Context'
 import { CommentData } from '@/utils/Quiz'
 import React, { useContext } from 'react'
-
+import DeleteComment from './DeleteComment'
+import { UserContext } from '@/utils/Context'
+import Image from 'next/image'
 const CommentCard = ({ comment }: { comment: CommentData }) => {
+  const { userData } = useContext(UserContext)
   return (
     <div
       key={comment.CommentID}
       className="p-4 bg-white border border-gray-200 rounded-lg shadow-sm"
     >
       <div className="flex sm:flex-row flex-col justify-between sm:items-center mb-4">
-        <div className=" flex items-center">
-          <img
-            src={
-              'https://english.cdn.zeenews.com/sites/default/files/2022/05/02/1038115-alia-bhatt-hania-aamir.jpg'
-            }
-            width={40}
-            height={40}
-            alt=""
-            className="rounded-full mr-4"
-          />{' '}
+        <div className="flex items-center gap-2">
+          <Image
+            src={comment.UserImage}
+            width={20}
+            height={20}
+            alt={comment.UserName}
+            className="rounded-full "
+          />
           <h4 className="text-lg font-semibold">{comment.UserName}</h4>
         </div>
         <div>
-          {' '}
           <span className="text-sm text-gray-500">
             {new Date(comment.CreatedAt).toLocaleDateString('en-US', {
               year: 'numeric',
@@ -32,7 +31,10 @@ const CommentCard = ({ comment }: { comment: CommentData }) => {
           </span>
         </div>
       </div>
-      <p className="text-gray-700">{comment.Text}</p>
+      <p className="text-gray-700 mb-4">{comment.Text}</p>
+      {userData.email === comment.UserEmail && (
+        <DeleteComment ID={comment.CommentID} />
+      )}
     </div>
   )
 }
