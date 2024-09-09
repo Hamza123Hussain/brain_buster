a04409ee2805daefa3968c11b3f265110949f72e
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-10 04:00:27 +0500

    added a disclaimer type

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,12 @@ import { fetchAllAiQuiz } from '@/functions/Quiz/GettingALLQuiz'
 import DisplayCard from '@/components/Quiz/DisplayCard'
 import Image from 'next/image'
 import { FaRegFrown, FaRegSadTear } from 'react-icons/fa'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
   const { userData, loading, setLoading, setscore } = useContext(UserContext)
   const [QuizData, SetQuizData] = useState<Quiz[]>([])
-
+  const Router = useRouter()
   useEffect(() => {
     const GetQuizzes = async () => {
       setLoading(true)
@@ -34,6 +35,16 @@ export default function Home() {
 
   return (
     <div className="flex flex-col justify-center items-center mx-auto p-4">
+      <div className=" flex justify-center items-center flex-col my-2">
+        {' '}
+        <span>Login To Have A Smooth Experience</span>
+        <button
+          onClick={() => Router.push('/Login')}
+          className="bg-[#4CAF50] px-7 border-[#FFFFFF] hover:bg-[#FFFFFF] hover:text-[#1C1C1C] text-[#FFFFFF] rounded-lg"
+        >
+          Login
+        </button>
+      </div>
       <div className="flex flex-col items-start my-4 justify-start p-4 rounded-xl bg-gray-800 text-white">
         <h3 className="capitalize text-2xl sm:text-4xl font-extrabold text-green-400">
           All Quizzes
